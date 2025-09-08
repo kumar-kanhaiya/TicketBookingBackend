d52f228b20b67cdbb670d3e97634427a2222c3c7
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-08 19:42:40 +0530

    code

--- src/main/java/TicketBooking/IRCTC/Services/UserBookingService.java
+++ src/main/java/TicketBooking/IRCTC/Services/UserBookingService.java
@@ -1,12 +1,30 @@
 package TicketBooking.IRCTC.Services;
 
 import TicketBooking.IRCTC.Entities.User;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
 
 public class UserBookingService {
 
     private User user;
 
-    public UserBookingService(User user){
+    private List<User> userList;
+
+    private static final String USERS_PATH= "../LocalDB/user.json";
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+
+    public UserBookingService(User user) throws IOException {
         this.user = user;
+        File users = new File(USERS_PATH);
+        userList = objectMapper.readValue(users, new TypeReference<List<User>>(){});
+
     }
 }
