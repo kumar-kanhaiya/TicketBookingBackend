9069f98302d04cd2110e0ba7b80e78b2a38b7144
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-08 14:22:21 +0530

    code

---
+++ src/main/java/TicketBooking/IRCTC/Entities/user.java
@@ -0,0 +1,8 @@
+package TicketBooking.IRCTC.Entities;
+
+public class user {
+
+    String name ;
+    String hashedPassword ;
+
+}
---
+++ src/test/java/TicketBooking/IRCTC/IrctcApplicationTests.java
@@ -0,0 +1,13 @@
+package TicketBooking.IRCTC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrctcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
