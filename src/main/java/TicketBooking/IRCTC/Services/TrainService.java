fe4941d7f59397daf3ed7b7ef00bf4c745238720
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-09 12:13:06 +0530

    code

--- src/main/java/TicketBooking/IRCTC/Services/TrainService.java
+++ src/main/java/TicketBooking/IRCTC/Services/TrainService.java
@@ -3,12 +3,22 @@ package TicketBooking.IRCTC.Services;
 import TicketBooking.IRCTC.Entities.Train;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TrainService {
 
     private List<Train> trainList;
 
     public searchTrains(String source , String dest){
-        return trainList.stream()filter(train -> validTrain(train , source , dest)).coll;
+        return trainList.stream()filter(train -> validTrain(train , source , dest)).collect(Collectors.toList());
+    }
+
+    private boolean validTrain(Train train , String source , String destination){
+        List<String> stationOrder = train.getStations();
+
+        int sourceIndex = stationOrder.indexOf(destination.toLowerCase());
+        int destinationIndex = stationOrder.indexOf(destination.toLowerCase());
+
+        return sourceIndex != -1 && destinationIndex != -1 && sourceIndex < destinationIndex;
     }
 }
